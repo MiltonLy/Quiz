d885875ea1f4e7f7dce3cbf6ac0fa827fe00acd6
Milton Ly <ly.milton@gmail.com>
2023-01-05 19:14:30 -0800

    finished the question selection

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
 var startBtnEl = document.getElementById('startBtn');
 var timerEl = document.getElementById('timer');
+var constainerEl = document.getElementById('container');
 var questionsEl = document.getElementById('question');
 var answersEl = document.getElementById('answers')
-var shufflePrompt;
-var currentPrompt;
+let shufflePrompt;
+let currentPrompt;
+var timeLeft = 75
 
 function countdown() {
-    var timeLeft = 75;
   
     var timeInterval = setInterval(function () {
       if (timeLeft > 1) {
@@ -22,14 +23,66 @@ function countdown() {
     }, 1000);
   }
 
-startBtnEl.addEventListener("click", function(){
-    startBtnEl.style.display = "none" 
-    shufflePrompt = prompts.sort(()=>Math.random - .5);
+startBtnEl.addEventListener("click", startGame)
+
+ function startGame(){
+    startBtnEl.style.display = "none";
+    //constainerEl.classList.remove('hide');
+    shufflePrompt = questions.sort(()=>Math.random - .5);
     currentPrompt = 0
     countdown();
-})
+    setPrompt();
+}
 
-var prompts = [
+function setPrompt(){
+    reset()
+    nextPrompt(shufflePrompt[currentPrompt])
+}
+
+function nextPrompt(question){
+    questionsEl.innerText = question.question
+    question.answer.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct){
+            button.dataset.correct = answer.correct;
+        } 
+        button.addEventListener('click', selectAnswer)
+        answersEl.appendChild(button);
+    })
+}
+
+function reset(){
+    while (answersEl.firstChild){
+        answersEl.removeChild(answersEl.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedAnswer = e.target
+    const correct = selectedAnswer.dataset.correct
+    statusClass(document.body.children)
+    Array.from(answersEl.children).forEach(button =>{
+        statusClass(button.dataset.correct)
+    })
+}
+
+function statusClass(element,correct){
+    clearStatusClass(element)
+    if (correct){
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.add('correct')
+    element.classList.add('wrong')
+}
+
+const questions = [
     {
         question: "Where do you modify the background color on the page?",
         answer: [
@@ -38,13 +91,14 @@ var prompts = [
             {text:"javascript", correct: false},
             {text:"jquery", correct: false},
         ]
+    },
+    {
+        question: "How would you connect your script.js to your html?",
+        answer: [
+            {text:"a tag", correct: false},
+            {text:"link tag", correct: false},
+            {text:"div tag", correct: false},
+            {text:"script tag", correct: true}
+        ]
     }
 ]
-
-function nextPrompt(){
-    nextQuestion(shufflePrompt[currentPrompt])
-}
-
-function nextQuestion(){
-
-}
