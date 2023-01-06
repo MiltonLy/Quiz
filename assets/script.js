8bcaf1f4047ca220f7cd403c0e8866530eb00daf
Milton Ly <ly.milton@gmail.com>
2023-01-05 21:46:31 -0800

    all questions added

--- assets/script.js
+++ assets/script.js
@@ -100,5 +100,77 @@ const questions = [
             {text:"div tag", correct: false},
             {text:"script tag", correct: true}
         ]
+    },
+    {
+        question: "Which css property would you use to show a container?",
+        answer: [
+            {text:"justify-content", correct:false},
+            {text:"flex", correct: false},
+            {text:"display", correct: true},
+            {text:"font", correct: false}
+        ]
+    },
+    {
+        question: "What css property would you use to make a shadow?",
+        answer: [
+            {text:"shadow", correct: true},
+            {text:"black marker", correct: false},
+            {text:"paint", correct: false},
+            {text:"fountain pen",correct: false}
+        ]
+    },
+    {
+        question: "How would you link css to your html?",
+        answer: [
+            {text:"link",correct: true},
+            {text:"script", correct: false},
+            {text:"article", correct: false},
+            {text:"div", correct: false}
+        ]
+    },
+    {
+        question: "What does refactoring mean?",
+        answer: [
+            {text:"cutting up a snippit of code",correct: false},
+            {text:"copying code", correct: false},
+            {text:"cleaning up code", correct: false},
+            {text:"delete code", correct: false}
+        ]
+    },
+    {
+        question: "What are normally at the top of a page?",
+        answer: [
+            {text:"article", correct: false},
+            {text:"body", correct:  false},
+            {text:"title", correct: false},
+            {text:"nav bar", correct: true}
+        ]
+    },
+    {
+        question: "What is an array?",
+        answer: [
+            {text:"a random number", correct: false},
+            {text:"a list of items",correct: true},
+            {text:"a variable", correct: false},
+            {text:"a JSON", correct: false}
+        ]
+    },
+    {
+        question: "Where can you manipulate the webpage?",
+        answer: [
+            {text:"python",correct: false},
+            {text:"css", correct: false},
+            {text:"html", correct: false},
+            {text:"javascript", correct: true}
+        ]
+    },
+    {
+        question: "What does JSON do?",
+        answer: [
+            {text:"creates an alert", correct: false},
+            {text:"take an array and makes it a string",correct: true},
+            {text:"grab an object from the html by their ID",correct: false},
+            {text:"derulo", correct: false}
+        ]
     }
 ]
